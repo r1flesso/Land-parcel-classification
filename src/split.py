dfa317781396248192d12e0a31ad835ffad0ce7c
Timur Allayarov <allayarov_tank100@mail.ru>
2026-05-04 16:05:27 +0400

    Create split.py

---
+++ src/split.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_csv('clear_data.csv')
+
+X = df.drop(columns=['id_2'])
+y = df['id_2']
+
+X_train, X_val_test, y_train, y_val_test = train_test_split(
+    X, y, train_size=0.7, stratify=y, random_state=42
+)
+
+X_val, X_test, y_val, y_test = train_test_split(
+    X_val_test, y_val_test, test_size=0.66, stratify=y_val_test, random_state=42
+)
